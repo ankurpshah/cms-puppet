ca05677de3d2fe4fde2f838e31e8e7b9a8c0682e
Andrei Serdeliuc <andrei@serdeliuc.ro>
2011-11-22 08:42:20 +0000

    missed a reload apache

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -19,7 +19,7 @@ class apache {
   			exec { "/usr/sbin/a2enmod $name":
   				unless => "/bin/sh -c '[ -L /etc/apache2/mods-enabled/${name}.load ] \\
   					&& [ /etc/apache2/mods-enabled/${name}.load -ef /etc/apache2/mods-available/${name}.load ]'",
-  				notify => Exec["force-reload-apache"],
+  				notify => Exec["force-reload-apache2"],
   				require => Package[$require_package],
   			}
   		}
@@ -27,7 +27,7 @@ class apache {
   			exec { "/usr/sbin/a2dismod $name":
   				onlyif => "/bin/sh -c '[ -L /etc/apache2/mods-enabled/${name}.load ] \\
   					&& [ /etc/apache2/mods-enabled/${name}.load -ef /etc/apache2/mods-available/${name}.load ]'",
-  				notify => Exec["force-reload-apache"],
+  				notify => Exec["force-reload-apache2"],
   				require => Package["apache"],
   			}
   		}
