5a76494cf53b6c06d9a2e6800ac189cdde83615e
Dan Frost <dan@danfrost.co.uk>
2011-12-09 12:31:28 +0000

    Added user 'dan' to base manifest

--- manifests/base.pp
+++ manifests/base.pp
@@ -27,6 +27,20 @@ class base {
         type    => 'rsa',
         key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC+gR9RC5KR5iM5iUp5ebikeGNSzs++8m0aZxfAiP+IMMFGFmUi4eRJLeet6rKxdHW0gM/r2OU9FSCvzbYS7CrBYfB8Tn3H/z1Z3eguGJxycaPJTbrzzXiGqZ6h+xH0cXpgb4Agv/914oBf/bie94kG7O5VKfz8bGTtVhKdqa/yFQ2DnEbhcs3f3zUr5A2taoIQhoV0ezBkAkpVXPAobP776Klp9U1r1YCDBJrdphHJ+vEMiJhVCp+TmUwI9r4oaRz534rdKuVvEAhguRehSGHTw5SX/qrqPPwL6Tg0XueL94J0HWOhvL4Qn973EDHI6csHlXKaX3ASys50bMZk/7IL'
     }
+
+    user::create { "dan": 
+        groups => $user::admin_group,
+        uid => 10001,
+        require => Class["user"]
+    }
+
+    ssh_authorized_key { "dan":
+        ensure  => present,
+        require => User['dan'],
+        user    => 'dan',
+        type    => 'rsa',
+        key     => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAtwzmQHIQTMK1XUjuiwCDzniZKhUtFX9HYON5z8LPOnetO9RA75opsDp0kawVhl+Gp8lg4pZeVL7IYQVp+q1S0PHfEJ/OfFJxHLKSU+DAbrS9rnBjsOQb/+YQr3W/DQzTUFxfUwgTcilkwmnw3Brr9S2mf+5mLgK1IBPmia+M6IM2cV5oedM8EDd4ETLR7vCVrupIjKL79uITPshQHGmOJcFcpGwsMVBOhPEeHM+9ye+j7ATcoLo2a+kKCtvUBJUlJ3xIuIBBv/YJ2uGTAuVvD7w4tTvprv1p5dFnkKwRXpiH/RVquqaofwMwOxNLOGpYBhTIpRWumK18WGIIUzT5KQ=='
+    }
 }
 
 class { 'base': stage => pre }
